a5ed28d33b03804c4513ff90355523e696432f0c
t-shitamichi22 <fko2247000@outlook.jp>
2025-01-04 14:55:24 +0900

    25.01.04

--- portfolio2024/portfolio/urls.py
+++ portfolio2024/portfolio/urls.py
@@ -5,4 +5,5 @@ app_name = 'portfolio'
 
 urlpatterns = [
     path('',views.IndexView.as_view(),name='index'),
+    path('detail/<int:pk>',views.DetailView.as_view(),name='detail')
 ]
--- portfolio2024/portfolio/views.py
+++ portfolio2024/portfolio/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import ListView,DetailView
 
 from .models import System
 
 
 # Create your views here.
-class IndexView2(TemplateView):
-    template_name = "index.html"
-
-
 class IndexView(ListView):
     template_name = "index.html"
+    model = System
+
+class DetailView(DetailView):
+    template_name = "detail.html"
     model = System
