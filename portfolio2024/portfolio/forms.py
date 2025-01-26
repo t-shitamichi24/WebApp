3fb3c525b197e2ffd2ed23f02e9be44977b5fe13
t-shitamichi24 <fko2447000@outlook.jp>
2025-01-27 08:56:42 +0900

    25.01.27

--- portfolio2024/portfolio/forms.py
+++ portfolio2024/portfolio/forms.py
@@ -2,6 +2,11 @@ from django.forms import ModelForm
 from .models import System
 
 class Post(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(Post, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
+
     class Meta:
         model = System
         fields = ['system_name', 'system_summary', 'system_url', 'system_thumbnail', 'system_background']
